be98ca9308d3d80bac25982a5cfdea8ec79c15b4
raghunandankavi2010 <raghunandankavi2010@gmail.com>
2016-02-29 21:37:04 +0530

    Material Search Sample picked from google io 2015 code

---
+++ MaterialSearch/app/src/main/java/com/materialsearch/MainActivity.java
@@ -0,0 +1,50 @@
+package com.materialsearch;
+
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    String mQuery = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Search");
+        toolbar.setNavigationIcon(R.drawable.ic_drawer);
+        this.setSupportActionBar(toolbar);
+
+
+    }
+
+    public void search(View view) {
+
+            startActivity( new Intent(this,SearchActivity.class));
+        }
+
+
+
+
+
+}
---
+++ MaterialSearch/app/src/main/java/com/materialsearch/SearchActivity.java
@@ -0,0 +1,287 @@
+package com.materialsearch;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.LoaderManager;
+import android.app.SearchManager;
+import android.content.CursorLoader;
+import android.content.Intent;
+import android.content.Loader;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.NavUtils;
+import android.support.v4.app.TaskStackBuilder;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.graphics.drawable.DrawableCompat;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+
+
+public class SearchActivity extends AppCompatActivity {
+
+    private static final String TAG ="SearchActivity";
+    private static final String SCREEN_LABEL = "Search";
+    private static final String ARG_QUERY = "query";
+
+    private SearchView mSearchView;
+    private String mQuery = "";
+    private ListView mSearchResults;
+    private SimpleCursorAdapter mResultsAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        mSearchView = (SearchView) findViewById(R.id.search_view);
+        setupSearchView();
+        mSearchResults = (ListView) findViewById(R.id.search_results);
+        Toolbar toolbar = (Toolbar) this.findViewById(R.id.toolbar_actionbar);
+
+        Drawable up = DrawableCompat.wrap(ContextCompat.getDrawable(this, R.drawable.ic_up));
+        DrawableCompat.setTint(up, ContextCompat.getColor(this,R.color.app_body_text_2));
+        toolbar.setNavigationIcon(up);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navigateUpOrBack(SearchActivity.this, null);
+            }
+        });
+
+        String query = getIntent().getStringExtra(SearchManager.QUERY);
+        query = query == null ? "" : query;
+        mQuery = query;
+
+        if (mSearchView != null) {
+            mSearchView.setQuery(query, false);
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            doEnterAnim();
+        }
+
+        overridePendingTransition(0, 0);
+    }
+
+    /**
+     * As we only ever want one instance of this screen, we set a launchMode of singleTop. This
+     * means that instead of re-creating this Activity, a new intent is delivered via this callback.
+     * This prevents multiple instances of the search dialog 'stacking up' e.g. if you perform a
+     * voice search.
+     *
+     * See: http://developer.android.com/guide/topics/manifest/activity-element.html#lmode
+     */
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        if (intent.hasExtra(SearchManager.QUERY)) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            if (!TextUtils.isEmpty(query)) {
+                searchFor(query);
+                mSearchView.setQuery(query, false);
+            }
+        }
+    }
+
+    private void setupSearchView() {
+        SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
+        mSearchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        mSearchView.setIconified(false);
+        // Set the query hint.
+        mSearchView.setQueryHint(getString(R.string.search_hint));
+        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                mSearchView.clearFocus();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                searchFor(s);
+                return true;
+            }
+        });
+        mSearchView.setOnCloseListener(new SearchView.OnCloseListener() {
+            @Override
+            public boolean onClose() {
+                dismiss(null);
+                return false;
+            }
+        });
+        if (!TextUtils.isEmpty(mQuery)) {
+            mSearchView.setQuery(mQuery, false);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        dismiss(null);
+    }
+
+    public void dismiss(View view) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            doExitAnim();
+        } else {
+            ActivityCompat.finishAfterTransition(this);
+        }
+    }
+
+    /**
+     * On Lollipop+ perform a circular reveal animation (an expanding circular mask) when showing
+     * the search panel.
+     */
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void doEnterAnim() {
+        // Fade in a background scrim as this is a floating window. We could have used a
+        // translucent window background but this approach allows us to turn off window animation &
+        // overlap the fade with the reveal animation – making it feel snappier.
+        View scrim = findViewById(R.id.scrim);
+        scrim.animate()
+                .alpha(1f)
+                .setDuration(500L)
+                .setInterpolator(
+                        AnimationUtils.loadInterpolator(this, android.R.interpolator.fast_out_slow_in))
+                .start();
+
+        // Next perform the circular reveal on the search panel
+        final View searchPanel = findViewById(R.id.search_panel);
+        if (searchPanel != null) {
+            // We use a view tree observer to set this up once the view is measured & laid out
+            searchPanel.getViewTreeObserver().addOnPreDrawListener(
+                    new ViewTreeObserver.OnPreDrawListener() {
+                        @Override
+                        public boolean onPreDraw() {
+                            searchPanel.getViewTreeObserver().removeOnPreDrawListener(this);
+                            // As the height will change once the initial suggestions are delivered by the
+                            // loader, we can't use the search panels height to calculate the final radius
+                            // so we fall back to it's parent to be safe
+                            int revealRadius = ((ViewGroup) searchPanel.getParent()).getHeight();
+                            // Center the animation on the top right of the panel i.e. near to the
+                            // search button which launched this screen.
+                            Animator show = ViewAnimationUtils.createCircularReveal(searchPanel,
+                                    searchPanel.getRight(), searchPanel.getTop(), 0f, revealRadius);
+                            show.setDuration(250L);
+                            show.setInterpolator(AnimationUtils.loadInterpolator(SearchActivity.this,
+                                    android.R.interpolator.fast_out_slow_in));
+                            show.start();
+                            return false;
+                        }
+                    });
+        }
+    }
+
+    /**
+     * On Lollipop+ perform a circular animation (a contracting circular mask) when hiding the
+     * search panel.
+     */
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void doExitAnim() {
+        final View searchPanel = findViewById(R.id.search_panel);
+        // Center the animation on the top right of the panel i.e. near to the search button which
+        // launched this screen. The starting radius therefore is the diagonal distance from the top
+        // right to the bottom left
+        int revealRadius = (int) Math.sqrt(Math.pow(searchPanel.getWidth(), 2)
+                + Math.pow(searchPanel.getHeight(), 2));
+        // Animating the radius to 0 produces the contracting effect
+        Animator shrink = ViewAnimationUtils.createCircularReveal(searchPanel,
+                searchPanel.getRight(), searchPanel.getTop(), revealRadius, 0f);
+        shrink.setDuration(200L);
+        shrink.setInterpolator(AnimationUtils.loadInterpolator(SearchActivity.this,
+                android.R.interpolator.fast_out_slow_in));
+        shrink.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                searchPanel.setVisibility(View.INVISIBLE);
+                ActivityCompat.finishAfterTransition(SearchActivity.this);
+            }
+        });
+        shrink.start();
+
+        // We also animate out the translucent background at the same time.
+        findViewById(R.id.scrim).animate()
+                .alpha(0f)
+                .setDuration(200L)
+                .setInterpolator(
+                        AnimationUtils.loadInterpolator(SearchActivity.this,
+                                android.R.interpolator.fast_out_slow_in))
+                .start();
+    }
+
+    private void searchFor(String query) {
+
+        Bundle args = new Bundle(1);
+        if (query == null) {
+            query = "";
+        }
+        args.putString(ARG_QUERY, query);
+        // do search
+        mQuery = query;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (isFinishing()) {
+            overridePendingTransition(0, 0);
+        }
+    }
+
+    public void navigateUpOrBack(Activity currentActivity,
+                                        Class<? extends Activity> syntheticParentActivity) {
+        // Retrieve parent activity from AndroidManifest.
+        Intent intent = NavUtils.getParentActivityIntent(currentActivity);
+
+        // Synthesize the parent activity when a natural one doesn't exist.
+        if (intent == null && syntheticParentActivity != null) {
+            try {
+                intent = NavUtils.getParentActivityIntent(currentActivity, syntheticParentActivity);
+            } catch (PackageManager.NameNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (intent == null) {
+            // No parent defined in manifest. This indicates the activity may be used by
+            // in multiple flows throughout the app and doesn't have a strict parent. In
+            // this case the navigation up button should act in the same manner as the
+            // back button. This will result in users being forwarded back to other
+            // applications if currentActivity was invoked from another application.
+            currentActivity.onBackPressed();
+        } else {
+            if (NavUtils.shouldUpRecreateTask(currentActivity, intent)) {
+                // Need to synthesize a backstack since currentActivity was probably invoked by a
+                // different app. The preserves the "Up" functionality within the app according to
+                // the activity hierarchy defined in AndroidManifest.xml via parentActivity
+                // attributes.
+                TaskStackBuilder builder = TaskStackBuilder.create(currentActivity);
+                builder.addNextIntentWithParentStack(intent);
+                builder.startActivities();
+            } else {
+                // Navigate normally to the manifest defined "Up" activity.
+                NavUtils.navigateUpTo(currentActivity, intent);
+            }
+        }
+    }
+
+
+
+}
