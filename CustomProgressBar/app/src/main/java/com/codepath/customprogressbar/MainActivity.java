5b0a31b1bfd309bf4f28d6d49dc0bef2c89b2133
raghunandankavi2010 <raghunandankavi2010@gmail.com>
2017-01-27 15:02:09 +0530

    Custom Progress Bar

---
+++ CustomProgressBar/app/src/main/java/com/codepath/customprogressbar/MainActivity.java
@@ -0,0 +1,37 @@
+package com.codepath.customprogressbar;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.Random;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @InjectView(R.id.progressBar) GoalProgressBar progressBar;
+
+    private final Random random = new Random();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+
+        //progressBar.setGoal(100);
+
+        if (savedInstanceState == null) {
+            resetProgress();
+        }
+    }
+
+    @OnClick(R.id.resetProgressBtn)
+    public void resetProgress() {
+        int prog = random.nextInt(100);
+        progressBar.setProgress(20);
+    }
+}
