59f6613285867ce08fa07413b488409b4800f69b
Raghunandan Kavi <raghunandankavi2010@gmail.com>
2017-05-13 20:14:26 +0530

    Match name along with number in call log

--- ContactsDashBoard_Raghunandan/app/src/main/java/com/example/raghu/contactsdashboard_raghunandan/Model.java
+++ ContactsDashBoard_Raghunandan/app/src/main/java/com/example/raghu/contactsdashboard_raghunandan/Model.java
@@ -8,6 +8,7 @@ import android.provider.CallLog;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.telephony.PhoneNumberUtils;
 import android.util.Log;
 
 import java.text.SimpleDateFormat;
@@ -29,9 +30,10 @@ import io.reactivex.functions.Predicate;
  */
 
 public class Model {
-    
+
 
     private static Model ourInstance = new Model();
+    
 
     public static Model getInstance() {
         return ourInstance;
@@ -42,9 +44,9 @@ public class Model {
     private static final String TAG = "Model";
     private List<Contacts> contactsList = new ArrayList<>();
 
-    public List<Contacts> readAllContacts() {
+    private List<Contacts> readAllContacts() {
 
-        String contactNAME, contactID, contactphotoURI, contactNumber, contactEmail;
+        String contactNAME = null, contactID, contactphotoURI, contactNumber = null, contactEmail;
         ContentResolver cr = MyApplication.getInstance().getContentResolver();
         Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
                 null, null, null, null);
@@ -68,8 +70,12 @@ public class Model {
                 if (Integer.parseInt(cur.getString(cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
                     //Query phone here.
                     contactNumber = getPhone(cr, contactID);
+
                     if (Utils.checkNotEmpty(contactNumber)) {
                         contact = new Contacts();
+
+                        contactNumber = PhoneNumberUtils.formatNumber(contactNumber);
+
                         contact.setNumber(contactNumber);
                         contact.setName(contactNAME);
                         contact.setId(contactID);
@@ -83,19 +89,18 @@ public class Model {
                         contactsList.add(contact);
                     }
 
-
                 }
-
             }
+
 
             cur.close();
         }
 
-        Log.d(TAG, " Size" + contactsList.size());
+        Log.d(TAG, "Size" + contactsList.size());
         return contactsList;
     }
 
-    public String getPhone(ContentResolver cr, String contactID) {
+    private String getPhone(ContentResolver cr, String contactID) {
 
         String contactNumber = null;
         Cursor cursorPhone = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
@@ -119,7 +124,7 @@ public class Model {
         return contactNumber;
     }
 
-    public String getEmail(ContentResolver cr, String contactID) {
+    private String getEmail(ContentResolver cr, String contactID) {
         String email = null;
         Cursor emailCur = cr.query(
                 ContactsContract.CommonDataKinds.Email.CONTENT_URI,
@@ -141,12 +146,12 @@ public class Model {
     }
 
 
-    public Observable<Contacts> getCallLog(Contacts contact) {
+    private Observable<Contacts> getCallLog(Contacts contact) {
 
         if (ActivityCompat.checkSelfPermission(MyApplication.getInstance(), Manifest.permission.READ_CALL_LOG) == PackageManager.PERMISSION_GRANTED) {
 
-            String SELECTION =
-                    CallLog.Calls.CACHED_NAME + " LIKE ?";
+            String SELECTION = CallLog.Calls.CACHED_NAME + " LIKE ?"; /*+"AND " +*/
+            //CallLog.Calls.NUMBER + " LIKE ?";
             String[] mSelectionArgs = {contact.getName()};
 
             Cursor cursor = MyApplication.getInstance().getContentResolver().query(CallLog.Calls.CONTENT_URI, null, SELECTION, mSelectionArgs, null);
@@ -157,6 +162,7 @@ public class Model {
 
 
                 int numb = cursor.getColumnIndex(CallLog.Calls.NUMBER);
+
                 int type = cursor.getColumnIndex(CallLog.Calls.TYPE);
                 int totalduration = 0;
 
@@ -164,23 +170,31 @@ public class Model {
 
                     String name = cursor.getString(cursor.getColumnIndex(CallLog.Calls.CACHED_NAME));
                     String phNum = cursor.getString(numb);
+                    Log.d(TAG, "CallLog Name : " + name);
+                    Log.d(TAG, "CallLog Number : " + phNum);
+                    // you get call logs if call log name matches with phone contact name
+                    // However the number in the contact may have country code as 09
+                    // while in call log it may be +91. So compare both.
+                    // return true if they're identical enough for caller ID purposes
+                    if(PhoneNumberUtils.compare(contact.getNumber(),phNum)) {
 
-                    int callType = Integer.parseInt(cursor.getString(type));
-                    duration = cursor.getString(cursor
-                            .getColumnIndex(android.provider.CallLog.Calls.DURATION));
-                    callDate = cursor.getString(cursor
-                            .getColumnIndex(android.provider.CallLog.Calls.DATE));
-                    SimpleDateFormat formatter = new SimpleDateFormat(
-                            "dd-MMM-yyyy HH:mm", Locale.getDefault());
+                        int callType = Integer.parseInt(cursor.getString(type));
+                        duration = cursor.getString(cursor
+                                .getColumnIndex(android.provider.CallLog.Calls.DURATION));
+                        callDate = cursor.getString(cursor
+                                .getColumnIndex(android.provider.CallLog.Calls.DATE));
+                        SimpleDateFormat formatter = new SimpleDateFormat(
+                                "dd-MMM-yyyy HH:mm", Locale.getDefault());
 
-                    dateString = formatter.format(new Date(Long
-                            .parseLong(callDate)));
+                        dateString = formatter.format(new Date(Long
+                                .parseLong(callDate)));
 
-                    //Log.d(TAG, contact.getName() +" "  + duration);
-                    int dur = Integer.parseInt(duration);
-                    if (dur > 0) {
-                        totalduration = totalduration + dur;
+                        //Log.d(TAG, contact.getName() +" "  + duration);
+                        int dur = Integer.parseInt(duration);
+                        if (dur > 0) {
+                            totalduration = totalduration + dur;
 
+                        }
                     }
                 }
 
