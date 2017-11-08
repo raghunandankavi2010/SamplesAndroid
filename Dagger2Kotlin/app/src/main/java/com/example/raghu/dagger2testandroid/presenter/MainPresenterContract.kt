3eba92cb335776d721f0b9b791cc13c78d00c099
Raghunandan Kavi <raghunandankavi2010@gmail.com>
2017-11-08 16:22:24 +0530

    convert all .java files to kotlin.

---
+++ Dagger2Kotlin/app/src/main/java/com/example/raghu/dagger2testandroid/presenter/MainPresenterContract.kt
@@ -0,0 +1,26 @@
+package com.example.raghu.dagger2testandroid.presenter
+
+import com.example.raghu.dagger2testandroid.models.User
+
+/**
+ * Created by raghu on 29/7/17.
+ */
+
+interface MainPresenterContract {
+
+
+    interface View {
+
+        fun showData(user: User)
+
+
+    }
+
+    interface Presenter {
+
+        fun doSomething()
+
+        fun unSubscribe()
+    }
+}
+
---
+++ Dagger2Kotlin/app/src/main/java/com/example/raghu/dagger2testandroid/utils/Constants.kt
@@ -0,0 +1,13 @@
+package com.example.raghu.dagger2testandroid.utils
+
+/**
+ * Created by raghu on 5/8/17.
+ */
+
+class Constants {
+
+    companion object {
+        val BASE_URL = "https://api.myjson.com/"
+    }
+
+}
