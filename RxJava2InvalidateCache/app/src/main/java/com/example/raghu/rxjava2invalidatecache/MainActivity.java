effef9c91d331c8ee2a488aa7f0febd57345c08a
Raghunandan Kavi <raghunandankavi2010@gmail.com>
2019-05-20 17:05:40 +0530

    Update Rxjava versions , gradle along with migrating to androidx
    Update Rxjava versions , gradle along with migrating to androidx

--- RxJava2InvalidateCache/app/src/main/java/com/example/raghu/rxjava2invalidatecache/MainActivity.java
+++ RxJava2InvalidateCache/app/src/main/java/com/example/raghu/rxjava2invalidatecache/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.raghu.rxjava2invalidatecache;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
 
