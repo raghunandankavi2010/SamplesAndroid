1cddaaefba1b8172517ee5402af060832d0bb443
Raghunandan <raghunandankavi2010@gmail.com>
2019-12-27 13:43:21 +0530

    Convert to kotlin and use of androidx libs.

--- DataBindingAAC/app/src/androidTest/java/com/example/raghu/databindingaac/ExampleInstrumentedTest.java
+++ DataBindingAAC/app/src/androidTest/java/com/example/raghu/databindingaac/ExampleInstrumentedTest.java
@@ -2,7 +2,8 @@ package com.example.raghu.databindingaac;
 
 import android.content.Context;
 import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.runner.AndroidJUnit4;
+
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,7 +20,7 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("com.example.raghu.databindingaac", appContext.getPackageName());
     }
