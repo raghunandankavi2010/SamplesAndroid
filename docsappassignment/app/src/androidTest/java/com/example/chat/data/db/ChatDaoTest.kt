1714e3816ab4a5a09ed68c290b3318aa2e7d3ad3
Raghunandan Kavi <raghunandan2005@gmail.com>
2021-01-21 12:55:42 +0530

    Remove unused import.

--- docsappassignment/app/src/androidTest/java/com/example/chat/data/db/ChatDaoTest.kt
+++ docsappassignment/app/src/androidTest/java/com/example/chat/data/db/ChatDaoTest.kt
@@ -26,16 +26,15 @@ package com.example.chat.data.db
 
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
-import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.hamcrest.MatcherAssert.assertThat
 
 @RunWith(AndroidJUnit4::class)
 class ChatDaoTest {
