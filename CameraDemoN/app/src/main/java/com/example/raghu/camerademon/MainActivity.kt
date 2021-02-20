b2b178eb9eb4e602fa68d5f70f30f3e242a7e401
Raghunandan Kavi <raghunandan2005@gmail.com>
2021-02-20 11:25:18 +0530

    Convert code to kotlin and remove unwanted libs.

--- CameraDemoN/app/src/main/java/com/example/raghu/camerademon/MainActivity.kt
+++ CameraDemoN/app/src/main/java/com/example/raghu/camerademon/MainActivity.kt
@@ -1,158 +1,107 @@
-package com.example.raghu.camerademon;
+package com.example.raghu.camerademon
 
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Matrix;
-import android.media.MediaScannerConnection;
-import android.net.Uri;
-import android.os.Bundle;
-import android.os.Environment;
-import android.widget.ImageView;
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import android.media.MediaScannerConnection
+import android.net.Uri
+import android.os.Bundle
+import android.os.Environment
+import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts.TakePicture
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
+import androidx.exifinterface.media.ExifInterface
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.FileProvider;
-import androidx.exifinterface.media.ExifInterface;
+class MainActivity : AppCompatActivity() {
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
+    private var imageView: ImageView? = null
+    private var mCurrentPhotoPath: String? = null
+    private var photoURI: Uri? = null
 
-public class MainActivity extends AppCompatActivity {
-
-    private ImageView imageView;
-    private String mCurrentPhotoPath;
-    private Uri photoURI;
-
-    ActivityResultLauncher<Uri> takePicture = registerForActivityResult(new ActivityResultContracts.TakePicture(),
-            new ActivityResultCallback<Boolean>() {
-                @Override
-                public void onActivityResult(Boolean result) {
-                    if (result) {
-                        try {
-                            InputStream is = getContentResolver().openInputStream(photoURI);
-                            Bitmap bitmap = BitmapFactory.decodeStream(is);
-                            bitmap = rotateImageIfRequired(MainActivity.this, bitmap, photoURI);
-                            imageView.setImageBitmap(bitmap);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-
-                        MediaScannerConnection.scanFile(MainActivity.this,
-                                new String[]{photoURI.getPath()}, null,
-                                new MediaScannerConnection.OnScanCompletedListener() {
-                                    public void onScanCompleted(String path, Uri uri) {
-                                    }
-                                });
-                    }
-                }
+    private var takePicture = registerForActivityResult(TakePicture()
+    ) { result ->
+        if (result) {
+            try {
+                val `is` = contentResolver.openInputStream(photoURI!!)
+                var bitmap = BitmapFactory.decodeStream(`is`)
+                bitmap = rotateImageIfRequired(this@MainActivity, bitmap, photoURI)
+                imageView!!.setImageBitmap(bitmap)
+            } catch (e: IOException) {
+                e.printStackTrace()
             }
-    );
+            MediaScannerConnection.scanFile(this@MainActivity, arrayOf(photoURI!!.path), null
+            ) { path, uri ->
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        imageView = findViewById(R.id.captured_image);
-        dispatchTakePictureIntent();
-
+            }
+        }
     }
 
-    private void dispatchTakePictureIntent() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        imageView = findViewById(R.id.captured_image)
+        dispatchTakePictureIntent()
+    }
 
-        File photoFile = null;
+    private fun dispatchTakePictureIntent() {
+        var photoFile: File? = null
         try {
-            photoFile = createImageFile();
-        } catch (IOException ex) {
+            photoFile = createImageFile()
+        } catch (ex: IOException) {
             // Error occurred while creating the File
-            ex.printStackTrace();
-
+            ex.printStackTrace()
         }
         // Continue only if the File was successfully created
         if (photoFile != null) {
-            photoURI = FileProvider.getUriForFile(MainActivity.this,
+            photoURI = FileProvider.getUriForFile(this@MainActivity,
                     "com.example.raghu.camerademon.fileProvider",
-                    photoFile);
-            takePicture.launch(photoURI);
+                    photoFile)
+            takePicture.launch(photoURI)
         }
-
     }
 
-
-    private File createImageFile() throws IOException {
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
         // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.ENGLISH).format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = new File(String.valueOf(getExternalFilesDir(Environment.DIRECTORY_DCIM)));
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.ENGLISH).format(Date())
+        val imageFileName = "JPEG_" + timeStamp + "_"
+        val storageDir = File(getExternalFilesDir(Environment.DIRECTORY_DCIM).toString())
 
         // Save a file: path for use with ACTION_VIEW intents
-
-        File image =  File.createTempFile(
+        val image = File.createTempFile(
                 imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
-        mCurrentPhotoPath = image.getAbsolutePath();
-        return image;
+                ".jpg",  /* suffix */
+                storageDir /* directory */
+        )
+        mCurrentPhotoPath = image.absolutePath
+        return image
     }
 
-    private static Bitmap rotateImageIfRequired(Context context, Bitmap img, Uri selectedImage) throws IOException {
-
-        InputStream input = context.getContentResolver().openInputStream(selectedImage);
-        ExifInterface ei;
-        ei = new ExifInterface(input);
-
-        int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
-
-        switch (orientation) {
-            case ExifInterface.ORIENTATION_ROTATE_90:
-                return rotateImage(img, 90);
-            case ExifInterface.ORIENTATION_ROTATE_180:
-                return rotateImage(img, 180);
-            case ExifInterface.ORIENTATION_ROTATE_270:
-                return rotateImage(img, 270);
-            default:
-                return img;
+    companion object {
+        @Throws(IOException::class)
+        private fun rotateImageIfRequired(context: Context, img: Bitmap, selectedImage: Uri?): Bitmap {
+            val input = context.contentResolver.openInputStream(selectedImage!!)
+            val ei = ExifInterface(input!!)
+            return when (ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL)) {
+                ExifInterface.ORIENTATION_ROTATE_90 -> rotateImage(img, 90)
+                ExifInterface.ORIENTATION_ROTATE_180 -> rotateImage(img, 180)
+                ExifInterface.ORIENTATION_ROTATE_270 -> rotateImage(img, 270)
+                else -> img
+            }
         }
-    }
 
-    private static Bitmap rotateImage(Bitmap img, int degree) {
-        Matrix matrix = new Matrix();
-        matrix.postRotate(degree);
-        Bitmap rotatedImg = Bitmap.createBitmap(img, 0, 0, img.getWidth(), img.getHeight(), matrix, true);
-        img.recycle();
-        return rotatedImg;
-    }
-
-    // in case scaling is required
-    private void setPic() {
-        // Get the dimensions of the View
-        int targetW = imageView.getWidth();
-        int targetH = imageView.getHeight();
-
-        // Get the dimensions of the bitmap
-        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-        bmOptions.inJustDecodeBounds = true;
-
-        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
-
-        int photoW = bmOptions.outWidth;
-        int photoH = bmOptions.outHeight;
-
-        // Determine how much to scale down the image
-        int scaleFactor = Math.max(1, Math.min(photoW/targetW, photoH/targetH));
-
-        // Decode the image file into a Bitmap sized to fill the View
-        bmOptions.inJustDecodeBounds = false;
-        bmOptions.inSampleSize = scaleFactor;
-
-        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
-        imageView.setImageBitmap(bitmap);
+        private fun rotateImage(img: Bitmap, degree: Int): Bitmap {
+            val matrix = Matrix()
+            matrix.postRotate(degree.toFloat())
+            val rotatedImg = Bitmap.createBitmap(img, 0, 0, img.width, img.height, matrix, true)
+            img.recycle()
+            return rotatedImg
+        }
     }
 }
