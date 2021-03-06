1ec18b88f5334e70461e2c1942222ecbd5605165
Raghunandan Kavi <raghunandan2005@gmail.com>
2021-03-06 14:43:32 +0530

    Add Shrink animation and reverse the same and repeat infinite.

--- CircleRipple/app/src/main/java/com/example/circleripple/PulseAnimationView.kt
+++ CircleRipple/app/src/main/java/com/example/circleripple/PulseAnimationView.kt
@@ -1,31 +1,29 @@
 package com.example.circleripple
 
-import kotlin.jvm.JvmOverloads
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
-import com.example.circleripple.PulseAnimationView
-import android.view.animation.LinearInterpolator
-import androidx.interpolator.view.animation.LinearOutSlowInInterpolator
 import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
-import android.view.MotionEvent
 import android.view.View
 import android.view.animation.Animation
+import android.view.animation.LinearInterpolator
+import androidx.interpolator.view.animation.LinearOutSlowInInterpolator
+
 
 class PulseAnimationView @JvmOverloads constructor(context: Context?, attrs: AttributeSet? = null) :
     View(context, attrs) {
 
     private var mRadius = 200f
     private val mPaint = Paint()
+    private val mPulseAnimatorSet = AnimatorSet()
 
     init {
         mPaint.apply {
             isAntiAlias = true
-            color = Color.RED
             style = Paint.Style.STROKE
             strokeWidth = 10f
         }
@@ -34,24 +32,37 @@ class PulseAnimationView @JvmOverloads constructor(context: Context?, attrs: Att
 
     public override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
 
+        // grow radius animation
         val growAnimator = ObjectAnimator.ofFloat(
-            this,
-            "radius", 0f, 200f
+                this,
+                "radius", 0f, 200f
         )
         growAnimator.duration = ANIMATION_DURATION.toLong()
         growAnimator.interpolator = LinearInterpolator()
-        growAnimator.repeatCount = Animation.INFINITE
-        growAnimator.start()
 
+        // shrink radius animation
+        val shrinkAnimator = ObjectAnimator.ofFloat(this,
+                "radius", 200f, 0f)
+        shrinkAnimator.duration = ANIMATION_DURATION.toLong()
+        shrinkAnimator.interpolator = LinearOutSlowInInterpolator()
+        shrinkAnimator.startDelay = ANIMATION_DELAY
+
+        // animation in reverse and repeat
+        val repeatAnimator = ObjectAnimator.ofFloat(this,
+                "radius", 0f, 200f)
+        repeatAnimator.startDelay = ANIMATION_DELAY
+        repeatAnimator.duration = ANIMATION_DURATION.toLong()
+        repeatAnimator.repeatCount = Animation.INFINITE
+        repeatAnimator.repeatMode = ValueAnimator.REVERSE
+
+        // play grow animation first and then shrink animation
+        // repeat grow after shrink
+        mPulseAnimatorSet.play(growAnimator).before(shrinkAnimator)
+        mPulseAnimatorSet.play(repeatAnimator).after(shrinkAnimator)
+        mPulseAnimatorSet.start()
     }
 
 
-    /**
-     * Required setter for the animated property.
-     * Called by the Animator to update the property.
-     *
-     * @param radius This view's radius property.
-     */
     fun setRadius(radius: Float) {
         mRadius = radius
         // Calculate a new color from the radius.
@@ -63,7 +74,7 @@ class PulseAnimationView @JvmOverloads constructor(context: Context?, attrs: Att
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        canvas.drawCircle(width/2.toFloat(), height/2.toFloat(), mRadius, mPaint)
+        canvas.drawCircle(width / 2.toFloat(), height / 2.toFloat(), mRadius, mPaint)
     }
 
     companion object {
